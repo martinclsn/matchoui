e5e8190ab4d50980b071ddd37833d629e37fb31a
martin <martin@clsn.se>
2014-05-11 23:11:41 +0200

    ignore log

--- scripts/installPlatformsAndPlugins.sh
+++ scripts/installPlatformsAndPlugins.sh
@@ -1,7 +1,9 @@
 #Install
-cordova plugin add org.apache.cordova.device
-cordova plugin add org.apache.cordova.console
-cordova plugin add org.apache.cordova.statusbar
+cordova platform add android
+cordova platform add ios
+#cordova plugin add org.apache.cordova.device
+#cordova plugin add org.apache.cordova.console
+#cordova plugin add org.apache.cordova.statusbar
 
 #Uninstall
 #cordova plugin remove org.apache.cordova.device
